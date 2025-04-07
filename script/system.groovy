4841041733e1dfa0f098fa30f5f3243ce4b7c45c
Prachi Shrivastava <shrivastavaprachi156@gmail.com>
2025-04-07 11:41:40 +0530

    Updated services and imporved naming conventions

--- script/system.groovy
+++
@@ -1,8 +0,0 @@
-def remoteMsg = ec.entity.makeValue("SystemMessageRemote")
-remoteMsg.set("description", "Test message")
-remoteMsg.set("sendUrl", "http://example.com")
-remoteMsg.set("username", "test_user")
-remoteMsg.set("password", "secret123")
-
-remoteMsg = remoteMsg.createOrUpdate()
-ec.message.addInfo("Created SystemMessageRemote ID: ${remoteMsg.systemMessageRemoteId}")
